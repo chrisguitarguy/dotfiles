3da59f5ce0f36aef7d3af60e7059ba7587f325e4
Christopher Davis <cdavis9999@gmail.com>
2012-04-29 01:24:11 -0500

    --group-directories-first ?? why yes.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -5,13 +5,13 @@
 alias refresh="source ~/.bash_profile"
 
 if [ ! 'Darwin' = $(uname) ]; then
-    alias ls="ls -1 --color"
-    alias l="ls -1 --color"
+    alias ls="ls -1 --color --group-directories-first"
+    alias l="ls"
     alias la="ls -al --color"
     alias lg="ls -al --color | grep "
 else
-    alias ls="ls -1"
-    alias l="ls -1"
+    alias ls="ls -1 --group-directories-first"
+    alias l="ls"
     alias la="ls -al"
     alias lg="ls -al | grep "
     alias flushdns="dscacheutil -flushcache"
